7326bb591c0cf1afe148fd164550caa837c2eb01
Kyel <kboisseau93@gmail.com>
2022-03-28 14:56:35 -0700

    Changes made to wireframe

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("hello")
+function greet (name){
+    document.write("hi,"+ name)
+}
+function favoritecharacter(){
+    let name=prompt("What is your favorite character from the office?")
+    document.write(name)
+}
