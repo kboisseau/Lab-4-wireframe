b3c361562ac7e52678a1c5f3b90e6c71fe47ca5a
Kyel <kboisseau93@gmail.com>
2022-03-28 16:25:43 -0700

    Additional changes

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ function favoritecharacter(){
     let name=prompt("What is your favorite character from the office?")
     document.write(name)
 }
+function officeseasons(){
+    let userAnswer=prompt(" How many seasons does the office have?");
+    if (userAnswer==9){
+        document.write("bazinga!")
+    }else{
+        officeseasons();
+    }
+    
+}
+function scrantonstrangler(){
+    let name=prompt("Who was the scranton strangler?")
+    document.write(name)
+}
